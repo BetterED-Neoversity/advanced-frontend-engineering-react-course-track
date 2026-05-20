4b49e23a76063fe17f20a37fb22c222910628c77
Replex <alexander.repeta@gmail.com>
2026-05-20 15:23:46 +0300

    module-01/lesson-01: add lesson plan and starter code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const jsxMarkup = (
+  <div>
+    <img src="https://i.imgflip.com/5glqrg.jpg" alt="Jackie" width={320} />
+    <p>HTML in JS? What kind of black magic is this? 🧙‍♂️</p>
+    <p>
+      This is <strong>JSX (JavaScript XML)</strong> — and yes, you really get
+      used to it.
+    </p>
+  </div>
+);
+
+createRoot(document.getElementById("root") as HTMLDivElement).render(jsxMarkup);
