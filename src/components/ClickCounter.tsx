cb5667c372cf3cd26003ea6b95d4cd7bcadf311a
Replex <alexander.repeta@gmail.com>
2026-05-20 15:29:12 +0300

    module-01/lesson-02: add lesson plan and starter code

---
+++ src/components/ClickCounter.tsx
@@ -0,0 +1,9 @@
+/**
+ * - Зберігає стан count
+ * - Збільшує поточне значення count при onClick
+ * - Рендериться у App
+ */
+
+export default function ClickCounter() {
+  return <button>{/* count */}</button>;
+}
