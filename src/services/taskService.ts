0de9ce12018a5ec592d0ab86a1ee2a400f58e7b6
Replex <alexander.repeta@gmail.com>
2026-05-21 16:18:08 +0300

    chore: replace MockAPI with json-server

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Task } from "../types/task";
 
-axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+axios.defaults.baseURL = "http://localhost:3001";
 
 export const getTasks = async () => {
   const res = await axios.get<Task[]>("/tasks");
